66df6b2d576392f669bf80a96293b45bef4d2885
Andrew Herrick <aj@andrewherrick.com>
2026-01-01 09:04:09 -0500

    Enable dark color mode in application startup
    Added Application.SetColorMode(SystemColorMode.Dark) to initialize the app in dark mode for improved user experience.

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ internal static class Program
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
+        Application.SetColorMode(SystemColorMode.Dark);
         Application.Run(new Form1());
     }
 }
